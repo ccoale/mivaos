44186d78f598720c65b24ff6e9b316c203cf1c5a
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-10 22:50:43 +0000

    Very basic boot loader works. Can "boot" from CD and display "Miva OS Boot Loader 0.1".

--- source/boot/boot.asm
+++ source/boot/boot.asm
@@ -9,7 +9,32 @@
 BITS 16 ; 16-bits until protected mode
 ORG 0x7C00 ; BIOS loads to this address
 
+JMP BootMain
 
+msg db "Miva OS Boot Loader 0.1", 0
+
+Print:
+	LODSB
+	OR al, al
+	JZ PrintDone
+	MOV ah, 0x0E
+	INT 0x10
+	JMP Print
+PrintDone:
+	RET
+
+BootMain:
+	XOR ax, ax
+	MOV ds, ax
+	MOV es, ax
+	
+	; print hello message
+	MOV si, msg
+	CALL Print
+	
+	cli
+	hlt
+	
 ; Make sure we are exactly 512 bytes in size
 ; This fills an entire sector
 TIMES 510-($-$$) DB 0 ; Fill rest of space with NULLs
