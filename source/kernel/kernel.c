4c743f30e619f5ecd8bbd38982e34663269a903d
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-11 21:27:41 +0000

    Changed video.c, video.h, memory.c, and memory.h. Completed video.c.

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -1,6 +1,8 @@
 #include "video.h" 
+#include "gdt.h"
 void kmain(void* mbd,unsigned int magic) 
 { 
+	gdt_install();
 cls(); 
 unsigned char*videoram=(unsigned char*)0xb8000; 
 videoram[0]=makeDefChar('a');
