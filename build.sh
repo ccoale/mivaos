4efaa740b43f1ef431395e2239cef91a5b420782
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-11 23:46:35 +0000

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm deploy/mivaos_x86.iso
 cp bin/kernel.bin deploy/boot/kernel.bin
-genisoimage -q -R -b deploy/boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o ./deploy/mivaos_x86.iso deploy/ 
+genisoimage -q -no-emul-boot -boot-load-size 4 -boot-info-table -R -b boot/grub/stage2_eltorito  -o ./deploy/mivaos_x86.iso ./deploy/
 1>/dev/null
