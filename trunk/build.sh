d3eb20879b71ed92cef94b5308bce654650cfec4
compgeek13 <compgeek13@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-11 20:50:58 +0000

    made the makefile call the iso build script to generate the iso

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,1 +1,2 @@
-genisoimage -R -b bin/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o deploy/mivaos_x86.iso ./
+genisoimage -q -R -b bin/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o ./deploy/mivaos_x86.iso ./ 
+1>/dev/null
