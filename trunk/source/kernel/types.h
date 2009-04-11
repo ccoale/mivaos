4a8b872e2cd2b2d2664ea732b0878c36027bce92
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-11 22:12:50 +0000

    Fixed types and memory.h

--- trunk/source/kernel/types.h
+++ trunk/source/kernel/types.h
@@ -3,13 +3,17 @@ basic types and defines
 */
 #ifndef __TYPES_H
 #define __TYPES_H
+
 typedef int BOOL;
 typedef unsigned char BYTE;
+typedef signed char SBYTE;
 typedef int size_t;
+
 typedef unsigned short USHORT;
 typedef USHORT WORD;
 typedef unsigned long ULONG;
 typedef ULONG DWORD;
 typedef unsigned int UINT;
 #define NULL 0x00
-#endif
+
+#endif /* __TYPES_H */
