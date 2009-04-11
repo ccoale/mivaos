69256580c7a044056d0d2ba886b0bf7f0284ba1a
compgeek13 <compgeek13@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-11 22:07:48 +0000

    fixed types, added function naming conventions

--- trunk/source/kernel/idt.h
+++ trunk/source/kernel/idt.h
@@ -4,7 +4,6 @@
 interupt descripter table API
 */
 #include "types.h"
-
-void idt_set_gate(UBYTE num,ulong base,ushort sel,UBYTE flags);
+void idt_set_gate(BYTE num,DWORD base,WORD sel,BYTE flags);
 void idt_install(void);
 #endif
