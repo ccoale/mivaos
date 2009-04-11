70f429b9d7a87e13860742e78ea579551b4957a4
compgeek13 <compgeek13@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-11 07:27:51 +0000

    added video functions

--- trunk/source/main.c
+++ trunk/source/main.c
@@ -1,7 +1,7 @@
-extern print(char* data);
-
+#include "include/video.h"
 void kmain(void* mbd,unsigned int magic)
 {
+cls();
 unsigned char*videoram=(unsigned char*)0xb8000;
 videoram[0]=65;
 videoram[1]=7;
