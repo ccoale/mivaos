8e0e3fa987de2f73c78811ad4ecdee9cd04f0478
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-12 03:15:41 +0000

    Updated build.sh

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rm deploy/mivaos_x86.iso
+rm -f deploy/mivaos_x86.iso
 cp bin/kernel.bin deploy/boot/kernel.bin
 genisoimage -q -no-emul-boot -boot-load-size 4 -boot-info-table -R -b boot/grub/stage2_eltorito  -o ./deploy/mivaos_x86.iso ./deploy/
-rm deploy/boot/kernel.bin
+rm -f deploy/boot/kernel.bin
 1>/dev/null
