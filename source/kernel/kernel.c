fac5a45b6aa5a0f3b881eacb12e3b55cc473714c
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-13 02:46:09 +0000

    changed ASCII header in kernel.c :)

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -6,16 +6,25 @@
 #include "keyboard.h"
 #include "boot.h"
 
+// makes our pretty ascii logo :)
+void OutputAsciiHeader()
+{
+	ConsolePuts(" ____    ____   _                   ___     ______   \n");
+	ConsolePuts("|_   \\  /   _| (_)                .'   `. .' ____ \\  \n");
+	ConsolePuts("  |   \\/   |   __  _   __  ,--.  /  .-.  \\| (___ \\_| \n");
+	ConsolePuts("  | |\\  /| |  [  |[ \\ [  ]`'_\\ : | |   | | _.____`.  \n");
+	ConsolePuts(" _| |_\\/_| |_  | | \\ \\/ / // | |,\\  `-'  /| \\____) | \n");
+	ConsolePuts("|_____||_____|[___] \\__/  \'-;__/ `.___.'  \\______.' \n");
+}
+
 void kmain(void* mbd,unsigned int magic)
 {
-struct MULTI_BOOT_INFO *boot=(struct MULTI_BOOT_INFO*)mbd;
+	struct MULTI_BOOT_INFO *boot=(struct MULTI_BOOT_INFO*)mbd;
 	ConsoleCls();
-ConsolePuts("              __  __\n");
-ConsolePuts(" /|/| \'   _  /  )(\n");
-ConsolePuts("/   |/ \\/(/ (__/__)\n\n");
-ConsolePuts("MivaOS written by Chris Coale <chris95219@gmail.com) and Tyler Littlefield <tyler@tysdomain.com>.\n");
-	ConsolePuts("MivaOS (kernel 0.0.1) Loading!\n");
-kprintf("Booting with %d KB lowmem and %d kb highmem.\n",boot->lowmem,boot->highmem);
+	OutputAsciiHeader();
+	ConsolePuts("\nMivaOS written by Chris Coale <chris95219@gmail.com) and Tyler Littlefield <tyler@tysdomain.com>.\n");
+	ConsolePuts("MivaOS (kernel 0.0.1) Loading...\n");
+	kprintf("Booting with %d KB lower memory and %d KB upper memory...\n",boot->lowmem,boot->highmem);
 	GdtInstall(); // setup gdt
 	IdtInstall(); // setup idt
 	IsrsInstall(); // setup isrs
@@ -25,6 +34,7 @@ kprintf("Booting with %d KB lowmem and %d kb highmem.\n",boot->lowmem,boot->high
 
 	// allows us to use our IRQs
 	__asm__ __volatile__ ("sti"); 
+
 	DWORD start = 0;
 
 	while (1)
