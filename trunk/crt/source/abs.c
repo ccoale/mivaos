3e917f3ac3582a325399c4b218a15b4c3c4a97fa
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-04-18 03:48:23 +0000

    Got rid of the custom CRT - we need to implement an already made one that conforms to standards.

--- trunk/crt/source/abs.c
+++
@@ -1,9 +0,0 @@
-/*
-** Finds the absolute value of an integer
-**
-*/
-#include "stdlib.h"
-
-int abs(int x) { return ( x < 0 ? -x : x ); }
-long int labs(long int x) { return ( x < 0 ? -x : x ); }
-long long int abs64(long long int x) { return ( x < 0 ? -x : x ); }
