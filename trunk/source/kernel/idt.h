2c81eac888afb54dc8116250305ca28cb4c9ac19
chris95219 <chris95219@64c38386-fd50-11dd-9fdb-3debd5008f28>
2009-09-21 01:04:40 +0000

    Got rid of -Werror in the makefile, and included memory.h in idt.h

--- trunk/source/kernel/idt.h
+++ trunk/source/kernel/idt.h
@@ -4,6 +4,7 @@ interupt descripter table API
 #ifndef __IDT_H
 #define __IDT_H
 #include "types.h"
+#include "memory.h"
 
 void IdtSetGate(BYTE num, DWORD base, WORD sel, BYTE flags);
 void IdtInstall(void);
